82c02ce4aef4c77d2a416e60c7e36eacb471a013
gabrielNetto94 <gabriel@moinhosul.com.br>
2024-12-04 08:26:45 -0300

    feat: add mock

--- api.ts
+++ api.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 import { ScheduleDay } from "./interface";
+import mock from "./mock.json";
 
 const url = "https://api.gripo.com.br/app/v1/schedule/available";
 export class Api {
   async getCourts(date: string, city_id: number, club_id: number) {
+    const scheduleDay: ScheduleDay = JSON.parse(JSON.stringify(mock));
+    return scheduleDay;
+
     const resp = await axios.post<ScheduleDay>(url, {
       date,
       city_id,
